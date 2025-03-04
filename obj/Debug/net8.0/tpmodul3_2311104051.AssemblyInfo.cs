feef52cdb9399fcc4958c13af1aee6b1c45feaaa
rizaldyzxz <aldyauliarahman16.com>
2025-03-04 11:59:20 +0700

    inisialisasi projek

--- obj/Debug/net8.0/tpmodul3_2311104051.AssemblyInfo.cs
+++ obj/Debug/net8.0/tpmodul3_2311104051.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul3_2311104051")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa2035af2f77c9d5dad85afd1ac10962d7bd769d")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul3_2311104051")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul3_2311104051")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
